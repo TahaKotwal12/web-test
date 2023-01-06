51bdeffe5e674b5b2d1dcd68ecc478392c817138
Taha Kotwal <107343425+TahaKotwal12@users.noreply.github.com>
2023-01-07 00:44:49 +0530

    Create nav.svelte

---
+++ src/nav.svelte
@@ -0,0 +1,32 @@
+<main>
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+  <div class="navigation">
+    <a href="WEBTEST" class="logo">webtest</a>
+    <a class="active" href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+    
+  </div>
+</main>
+
+<style>
+  .navigation{
+  max-width: 100%;
+  background-color: #333;
+  overflow: hidden;
+}
+ 
+.navigation a{
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  transition: 0.4s;
+}
+.navigation a:hover{
+  color: #f46d13;
+}
+</style>
